7ea66ebfe4063b694844a2971dd361b3d1933bd9
Joe M <bs.github@burnsoft.net>
2021-03-29 18:38:21 -0400

    #3 Started class to handle Maintance_Details table

---
+++ BurnSoft.Applications.MGC/Firearms/MaintanceDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.Firearms
+{
+    /// <summary>
+    /// Class MaintanceDetails that will help manage the details of teh Maintance_Details table.
+    /// </summary>
+    public class MaintanceDetails
+    {
+    }
+}
