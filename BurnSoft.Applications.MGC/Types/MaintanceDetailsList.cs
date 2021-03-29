2d91987a0be3631eb045fbe6d954fd4680dc1fa2
Joe M <bs.github@burnsoft.net>
2021-03-29 18:44:06 -0400

    #3 Added list types to store the information

---
+++ BurnSoft.Applications.MGC/Types/MaintanceDetailsList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.Types
+{
+    /// <summary>
+    /// Class MaintanceDetailsList to store the information from the Maintance_Details table
+    /// </summary>
+    [Serializable]
+    public class MaintanceDetailsList
+    {
+        public long Id { get; set; }
+        public long GunId { get; set; }
+        public long PlanId { get; set; }
+
+    }
+}
---
+++ BurnSoft.Applications.MGC/Types/MaintancePlans.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.Types
+{
+    /// <summary>
+    /// Class MaintancePlans to hold the information from the Maintance_Plans table
+    /// </summary>
+    [Serializable]
+    public class MaintancePlans
+    {
+        public long Id { get; set; }
+
+    }
+}
