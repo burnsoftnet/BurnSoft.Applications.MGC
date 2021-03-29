b5c3ae04ded49ad1e6e350db39d2870168bd002e
Joe M <bs.github@burnsoft.net>
2021-03-29 19:25:44 -0400

    #3  finished maintance plans list container

--- BurnSoft.Applications.MGC/Types/MaintancePlans.cs
+++ BurnSoft.Applications.MGC/Types/MaintancePlans.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BurnSoft.Applications.MGC.Types
 {
@@ -12,7 +8,36 @@ namespace BurnSoft.Applications.MGC.Types
     [Serializable]
     public class MaintancePlans
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
         public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the operation details. is od in the table
+        /// </summary>
+        /// <value>The operation details.</value>
+        public string OperationDetails { get; set; }
+        /// <summary>
+        /// Gets or sets the intervals in days. iid in table
+        /// </summary>
+        /// <value>The intervals in days.</value>
+        public string IntervalsInDays { get; set; }
+        /// <summary>
+        /// Gets or sets the interval in rounds fired. is iirf in table
+        /// </summary>
+        /// <value>The interval in rounds fired.</value>
+        public string IntervalInRoundsFired { get; set; }
+        /// <summary>
+        /// Gets or sets the notes.
+        /// </summary>
+        /// <value>The notes.</value>
+        public string Notes { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize. sync_lastupdate in table
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
 
     }
 }
