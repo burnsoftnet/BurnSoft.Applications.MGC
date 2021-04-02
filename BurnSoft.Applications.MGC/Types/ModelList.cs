ccdd42b7afaf0e30d5903207251ab585565a65d4
Joe M <bs.github@burnsoft.net>
2021-04-02 10:04:29 -0400

    #5 Added list container type for models

---
+++ BurnSoft.Applications.MGC/Types/ModelList.cs
@@ -0,0 +1,30 @@
+﻿
+namespace BurnSoft.Applications.MGC.Types
+{
+    /// <summary>
+    /// Class ModelList which will contain all the information gathered form teh Gun_Model table
+    /// </summary>
+    public class ModelList
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the manufacturer identifier.  This is GMID in the tabl
+        /// </summary>
+        /// <value>The manufacturer identifier.</value>
+        public long ManufacturerId { get; set; }
+        /// <summary>
+        /// Gets or sets the name. Is field is Model in the table.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize. This is sync_lastupdate in the table.
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
