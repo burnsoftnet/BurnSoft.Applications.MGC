f298826162f6c99f31e65bc86796bc5a0e67a4e0
Joe M <bs.github@burnsoft.net>
2021-04-02 10:22:57 -0400

    #5  finished the rest of the functions for this class

--- BurnSoft.Applications.MGC/Firearms/Models.cs
+++ BurnSoft.Applications.MGC/Firearms/Models.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BurnSoft.Applications.MGC.Types;
+
 // ReSharper disable UnusedMember.Local
 
 namespace BurnSoft.Applications.MGC.Firearms
@@ -122,6 +124,131 @@ namespace BurnSoft.Applications.MGC.Firearms
                 errOut = ErrorMessage("Exists", e);
             }
             return bAns;
+        }
+
+        public static bool Delete(string databasePath, long id, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                string sql = $"delete from Gun_Model where id={id}";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Delete", e);
+            }
+            return bAns;
+        }
+
+        public static long GetId(string databasePath, string name, long manufacturerId, out string errOut)
+        {
+            long lAns = 0;
+            errOut = @"";
+            try
+            {
+                string sql = $"SELECT * from  Gun_Model where Model='{name}' and GMID={manufacturerId}";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                foreach (DataRow d in dt.Rows)
+                {
+                    lAns = Convert.ToInt32(d["id"]);
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetId", e);
+            }
+            return lAns;
+        }
+        public static string GetName(string databasePath, long id, out string errOut)
+        {
+            string sAns = @"";
+            errOut = @"";
+            try
+            {
+                string sql = $"SELECT * from Gun_Model where id={id}";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                List<ModelList> lst = MyList(dt, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                foreach (ModelList l in lst)
+                {
+                    sAns = l.Name;
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetName", e);
+            }
+            return sAns;
+        }
+
+        public static List<ModelList> Lists(string databasePath, long manufacturerId, out string errOut)
+        {
+            List<ModelList> lst = new List<ModelList>();
+            errOut = @"";
+            try
+            {
+                string sql = $"SELECT * from  Gun_Model where GMID={manufacturerId}";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                lst = MyList(dt, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Lists", e);
+            }
+
+            return lst;
+        }
+
+        public static List<ModelList> Lists(string databasePath, out string errOut)
+        {
+            List<ModelList> lst = new List<ModelList>();
+            errOut = @"";
+            try
+            {
+                string sql = $"SELECT * from  Gun_Model";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                lst = MyList(dt, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Lists", e);
+            }
+
+            return lst;
+        }
+
+        private static List<ModelList> MyList(DataTable dt, out string errOut)
+        {
+            List<ModelList> lst = new List<ModelList>();
+            errOut = @"";
+            try
+            {
+                foreach (DataRow d in dt.Rows)
+                {
+                    lst.Add(new ModelList()
+                    {
+                        Id = Convert.ToInt32(d["id"]),
+                        ManufacturerId = Convert.ToInt32(d["GMID"]),
+                        Name = d["Model"] != DBNull.Value ? d["Model"].ToString() : "",
+                        LastSync = d["sync_lastupdate"] != DBNull.Value ? d["sync_lastupdate"].ToString() : ""
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("MyList", e);
+            }
+
+            return lst;
         }
     }
 }
