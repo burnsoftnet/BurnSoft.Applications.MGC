d23ef739c1a2d8865f1a4090cda71fef62c995ff
Joe M <bs.github@burnsoft.net>
2021-04-08 15:57:12 -0400

    #9 Started unit test for wishlist

---
+++ BurnSoft.Applications.MGC.UnitTest/Other/WishlistTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BurnSoft.Applications.MGC.UnitTest.Other
+{
+    [TestClass]
+    public class WishlistTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
