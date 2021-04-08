0997f89d7ddc96b004aa8070b5c3fb2aa18aa00d
Joe M <bs.github@burnsoft.net>
2021-04-08 11:14:47 -0400

    #9 nAdded list container for Wishlist table

---
+++ BurnSoft.Applications.MGC/Types/WishlistList.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace BurnSoft.Applications.MGC.Types
+{
+    /// <summary>
+    /// Class WishlistList. The list container for the data from the Wishlist table
+    /// </summary>
+    [Serializable]
+    public class WishlistList
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the manufacturer.
+        /// </summary>
+        /// <value>The manufacturer.</value>
+        public string Manufacturer { get; set; }
+        /// <summary>
+        /// Gets or sets the model.
+        /// </summary>
+        /// <value>The model.</value>
+        public string Model { get; set; }
+        /// <summary>
+        /// Gets or sets the placeto buy.
+        /// </summary>
+        /// <value>The placeto buy.</value>
+        public string PlacetoBuy { get; set; }
+        /// <summary>
+        /// Gets or sets the qty.
+        /// </summary>
+        /// <value>The qty.</value>
+        public string Qty { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Value { get; set; }
+        /// <summary>
+        /// Gets or sets the notes.
+        /// </summary>
+        /// <value>The notes.</value>
+        public string Notes { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize. Is called sync_lastupdate in the table
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
