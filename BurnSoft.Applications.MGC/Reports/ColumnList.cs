253f966ec3b2a59ee53bce8e55abc05ec0de3af7
Joe M <bs.github@burnsoft.net>
2021-04-14 18:35:28 -0400

    #14 Imported function from main app

--- BurnSoft.Applications.MGC/Reports/ColumnList.cs
+++ BurnSoft.Applications.MGC/Reports/ColumnList.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using BurnSoft.Applications.MGC.Types;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
 
 namespace BurnSoft.Applications.MGC.Reports
 {
@@ -51,6 +53,53 @@ namespace BurnSoft.Applications.MGC.Reports
         /// <param name="e">The e.</param>
         /// <returns>System.String.</returns>
         private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
-        #endregion        
+        #endregion
+
+        public static string GetColumnName(string databasePath, long tableId, string displayName, out string errOut)
+        {
+            string sAns = "";
+            errOut = @"";
+            try
+            {
+                string sql = $"SELECT col from CR_ColumnList where tid={tableId} and DN='{displayName}'";
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut?.Length > 0) throw new Exception(errOut);
+                foreach (DataRow d in dt.Rows)
+                {
+                    sAns = d["Col"].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetColumnName", e);
+            }
+
+            return sAns;
+        }
+
+        private static List<ColumnLists> MyList(DataTable dt, out string errOut)
+        {
+            List<ColumnLists> lst = new List<ColumnLists>();
+            errOut = @"";
+            try
+            {
+                foreach (DataRow d in dt.Rows)
+                {
+                    lst.Add(new ColumnLists()
+                    {
+                        Id = Convert.ToInt32(d["id"]),
+                        TableId = d["TID"] != DBNull.Value ? Convert.ToInt32(d["TID"]) : 0,
+                        Column = d["Col"] != DBNull.Value ? d["Col"].ToString() : "",
+                        DisplayName = d["DN"] != DBNull.Value ? d["DN"].ToString() : ""
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("MyList", e);
+            }
+
+            return lst;
+        }
     }
 }
--- BurnSoft.Applications.MGC/Types/ColumnLists.cs
+++ BurnSoft.Applications.MGC/Types/ColumnLists.cs
@@ -11,5 +11,25 @@ namespace BurnSoft.Applications.MGC.Types
     /// </summary>
     public class ColumnLists
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the table identifier. This is TID in the table
+        /// </summary>
+        /// <value>The table identifier.</value>
+        public long TableId { get; set; }
+        /// <summary>
+        /// Gets or sets the column. This is Col in the table
+        /// </summary>
+        /// <value>The column.</value>
+        public string Column { get; set; }
+        /// <summary>
+        /// Gets or sets the display name. This is DN in the table
+        /// </summary>
+        /// <value>The display name.</value>
+        public string DisplayName { get; set; }
     }
 }
