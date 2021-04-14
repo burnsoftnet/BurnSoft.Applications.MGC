4cae713e14633f4249d33eb206445ad85ca34dca
Joe M <bs.github@burnsoft.net>
2021-04-14 08:14:56 -0400

    #9 Finished Wishlist unit test

--- BurnSoft.Applications.MGC.UnitTest/Other/WishlistTest.cs
+++ BurnSoft.Applications.MGC.UnitTest/Other/WishlistTest.cs
@@ -21,25 +21,33 @@ namespace BurnSoft.Applications.MGC.UnitTest.Other
         /// </summary>
         private string _errOut;
         /// <summary>
-        /// The gun identifier
-        /// </summary>
-        private int _gunId;
-        /// <summary>
         /// The database path
         /// </summary>
         private string _databasePath;
-       
-        private string WishList_Manufacturer;
-        
-        private string WishList_Model;
-        
-        private string WishList_PlaceToBuy;
-        
-        private string WishList_Qty;
-        
-        private string WishList_Price;
-        
-        private string WishList_Notes;
+        /// <summary>
+        /// The wish list manufacturer
+        /// </summary>
+        private string _wishListManufacturer;
+        /// <summary>
+        /// The wish list model
+        /// </summary>
+        private string _wishListModel;
+        /// <summary>
+        /// The wish list place to buy
+        /// </summary>
+        private string _wishListPlaceToBuy;
+        /// <summary>
+        /// The wish list qty
+        /// </summary>
+        private string _wishListQty;
+        /// <summary>
+        /// The wish list price
+        /// </summary>
+        private string _wishListPrice;
+        /// <summary>
+        /// The wish list notes
+        /// </summary>
+        private string _wishListNotes;
 
         /// <summary>
         /// Initializes this instance.
@@ -51,13 +59,12 @@ namespace BurnSoft.Applications.MGC.UnitTest.Other
             BSOtherObjects obj = new BSOtherObjects();
             _errOut = @"";
             _databasePath = Vs2019.GetSetting("DatabasePath", TestContext);
-            _gunId = Vs2019.IGetSetting("MyGunCollectionID", TestContext);
-            WishList_Manufacturer = obj.FC(Vs2019.GetSetting("WishList_Manufacturer", TestContext));
-            WishList_Model = obj.FC(Vs2019.GetSetting("WishList_Model", TestContext));
-            WishList_PlaceToBuy = obj.FC(Vs2019.GetSetting("WishList_PlaceToBuy", TestContext));
-            WishList_Qty = obj.FC(Vs2019.GetSetting("WishList_Qty", TestContext));
-            WishList_Price = obj.FC(Vs2019.GetSetting("WishList_Price", TestContext));
-            WishList_Notes = obj.FC(Vs2019.GetSetting("WishList_Notes", TestContext));
+            _wishListManufacturer = obj.FC(Vs2019.GetSetting("WishList_Manufacturer", TestContext));
+            _wishListModel = obj.FC(Vs2019.GetSetting("WishList_Model", TestContext));
+            _wishListPlaceToBuy = obj.FC(Vs2019.GetSetting("WishList_PlaceToBuy", TestContext));
+            _wishListQty = obj.FC(Vs2019.GetSetting("WishList_Qty", TestContext));
+            _wishListPrice = obj.FC(Vs2019.GetSetting("WishList_Price", TestContext));
+            _wishListNotes = obj.FC(Vs2019.GetSetting("WishList_Notes", TestContext));
 
         }
         /// <summary>
@@ -65,9 +72,9 @@ namespace BurnSoft.Applications.MGC.UnitTest.Other
         /// </summary>
         private void VerifyDoesntExist()
         {
-            if (WishList.Exists(_databasePath, WishList_Manufacturer, WishList_Model, out _errOut))
+            if (WishList.Exists(_databasePath, _wishListManufacturer, _wishListModel, out _errOut))
             {
-                long id = WishList.GetId(_databasePath, WishList_Manufacturer, WishList_Model, out _errOut);
+                long id = WishList.GetId(_databasePath, _wishListManufacturer, _wishListModel, out _errOut);
                 WishList.Delete(_databasePath, id, out _errOut);
             }
         }
@@ -76,9 +83,9 @@ namespace BurnSoft.Applications.MGC.UnitTest.Other
         /// </summary>
         private void VerifyExists()
         {
-            if (!WishList.Exists(_databasePath, WishList_Manufacturer, WishList_Model, out _errOut))
+            if (!WishList.Exists(_databasePath, _wishListManufacturer, _wishListModel, out _errOut))
             {
-                WishList.Add(_databasePath, WishList_Manufacturer, WishList_Model, WishList_PlaceToBuy, WishList_Qty, WishList_Price, WishList_Notes,out _errOut);
+                WishList.Add(_databasePath, _wishListManufacturer, _wishListModel, _wishListPlaceToBuy, _wishListQty, _wishListPrice, _wishListNotes,out _errOut);
             }
         }
         /// <summary>
@@ -98,15 +105,91 @@ namespace BurnSoft.Applications.MGC.UnitTest.Other
                     TestContext.WriteLine($"Value: {v.Value}");
                     TestContext.WriteLine($"LastSync: {v.LastSync}");
                     TestContext.WriteLine($"Notes: {v.Notes}");
-                    TestContext.WriteLine($"");
-                    TestContext.WriteLine($"--------------------------");
-                    TestContext.WriteLine($"");
+                    TestContext.WriteLine("");
+                    TestContext.WriteLine("--------------------------");
+                    TestContext.WriteLine("");
                 }
             }
         }
-        [TestMethod]
-        public void TestMethod1()
+        /// <summary>
+        /// Defines the test method AddTest.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void AddTest()
         {
+            VerifyDoesntExist();
+            bool value = WishList.Add(_databasePath, _wishListManufacturer, _wishListModel, _wishListPlaceToBuy, _wishListQty, _wishListPrice, _wishListNotes, out _errOut);
+            General.HasTrueValue(value, _errOut);
+        }
+        /// <summary>
+        /// Defines the test method ExistsTest.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void ExistsTest()
+        {
+            VerifyExists();
+            bool value = WishList.Exists(_databasePath, _wishListManufacturer, _wishListModel, out _errOut);
+            General.HasTrueValue(value, _errOut);
+        }
+        /// <summary>
+        /// Defines the test method GetId.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void GetId()
+        {
+            VerifyExists();
+            long value = WishList.GetId(_databasePath, _wishListManufacturer, _wishListModel, out _errOut);
+            TestContext.WriteLine($"ID = {value}");
+            General.HasTrueValue(value > 0, _errOut);
+        }
+        /// <summary>
+        /// Defines the test method UpdateTest.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void UpdateTest()
+        {
+            VerifyExists();
+            long id = WishList.GetId(_databasePath, _wishListManufacturer, _wishListModel, out _errOut);
+
+            bool value = WishList.Update(_databasePath, id, _wishListManufacturer, _wishListModel, _wishListPlaceToBuy,
+                _wishListQty, _wishListPrice, $"UPDATE TEST {_wishListNotes}", out _errOut);
+            General.HasTrueValue(value, _errOut);
+        }
+        
+
+        /// <summary>
+        /// Defines the test method DeleteTest.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void DeleteTest()
+        {
+            VerifyExists();
+            long id = WishList.GetId(_databasePath, _wishListManufacturer, _wishListModel, out _errOut);
+            bool value = Accessories.Delete(_databasePath, id, out _errOut);
+            General.HasTrueValue(value, _errOut);
+        }
+        /// <summary>
+        /// Defines the test method ListByIDTest.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void ListByIdTest()
+        {
+            VerifyExists();
+            long id = WishList.GetId(_databasePath, _wishListManufacturer, _wishListModel, out _errOut);
+            List<WishlistList> value = WishList.List(_databasePath, (int)id, out _errOut);
+            PrintList(value);
+            General.HasTrueValue(value.Count > 0, _errOut);
+        }
+        /// <summary>
+        /// Defines the test method ListTest.
+        /// </summary>
+        [TestMethod, TestCategory("Wishlist")]
+        public void ListTest()
+        {
+            VerifyExists();
+            List<WishlistList> value = WishList.List(_databasePath, out _errOut);
+            PrintList(value);
+            General.HasTrueValue(value.Count > 0, _errOut);
         }
     }
 }
