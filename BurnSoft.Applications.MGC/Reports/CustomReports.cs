cb8da8639d1d0314c1dccafc14be5b075d85d557
Joe M <bs.github@burnsoft.net>
2021-04-14 14:18:43 -0400

    #1 #12 added comments

--- BurnSoft.Applications.MGC/Reports/CustomReports.cs
+++ BurnSoft.Applications.MGC/Reports/CustomReports.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BurnSoft.Applications.MGC.Types;
+// ReSharper disable UnusedMember.Local
 
 namespace BurnSoft.Applications.MGC.Reports
 {
@@ -53,7 +51,15 @@ namespace BurnSoft.Applications.MGC.Reports
         /// <param name="e">The e.</param>
         /// <returns>System.String.</returns>
         private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
-        #endregion
+        #endregion        
+        /// <summary>
+        /// Adds the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="reportName">Name of the report.</param>
+        /// <param name="mySql">My SQL.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         public static bool Add(string databasePath, string reportName, string mySql, out string errOut)
         {
             bool bAns = false;
@@ -71,7 +77,15 @@ namespace BurnSoft.Applications.MGC.Reports
 
             return bAns;
         }
-
+        /// <summary>
+        /// Updates the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="reportId">The report identifier.</param>
+        /// <param name="reportName">Name of the report.</param>
+        /// <param name="mySql">My SQL.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         public static bool Update(string databasePath,long reportId, string reportName, string mySql, out string errOut)
         {
             bool bAns = false;
@@ -89,7 +103,14 @@ namespace BurnSoft.Applications.MGC.Reports
 
             return bAns;
         }
-
+        /// <summary>
+        /// Existses the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <exception cref="Exception"></exception>
         public static bool Exists(string databasePath, string name, out string errOut)
         {
             bool bAns = false;
@@ -107,7 +128,14 @@ namespace BurnSoft.Applications.MGC.Reports
             }
             return bAns;
         }
-
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>System.Int64.</returns>
+        /// <exception cref="Exception"></exception>
         public static long GetId(string databasePath, string name, out string errOut)
         {
             long lAns = 0;
@@ -128,7 +156,13 @@ namespace BurnSoft.Applications.MGC.Reports
             }
             return lAns;
         }
-
+        /// <summary>
+        /// Deletes the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="reportId">The report identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
         public static bool Delete(string databasePath, long reportId, out string errOut)
         {
             bool bAns = false;
@@ -146,7 +180,15 @@ namespace BurnSoft.Applications.MGC.Reports
 
             return bAns;
         }
-
+        /// <summary>
+        /// Lists the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;CustomReportsLists&gt;.</returns>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="Exception"></exception>
         public static List<CustomReportsLists> List(string databasePath, long id, out string errOut)
         {
             List<CustomReportsLists> lst = new List<CustomReportsLists>();
@@ -167,7 +209,14 @@ namespace BurnSoft.Applications.MGC.Reports
 
             return lst;
         }
-
+        /// <summary>
+        /// Lists the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;CustomReportsLists&gt;.</returns>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="Exception"></exception>
         public static List<CustomReportsLists> List(string databasePath, out string errOut)
         {
             List<CustomReportsLists> lst = new List<CustomReportsLists>();
@@ -188,7 +237,12 @@ namespace BurnSoft.Applications.MGC.Reports
 
             return lst;
         }
-
+        /// <summary>
+        /// Mies the list.
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;CustomReportsLists&gt;.</returns>
         private static List<CustomReportsLists> MyList(DataTable dt, out string errOut)
         {
             List<CustomReportsLists> lst = new List<CustomReportsLists>();
