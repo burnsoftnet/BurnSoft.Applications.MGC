76487bc0bbd2d059d2a7378977ac4d79626b09c7
Joe M <bs.github@burnsoft.net>
2021-04-15 08:26:10 -0400

    #24 created classes to  handle the TableList information

---
+++ BurnSoft.Applications.MGC/Types/TableLists.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace BurnSoft.Applications.MGC.Types
+{
+    /// <summary>
+    /// Class TableLists.  List container for data from the CR_TableList table.
+    /// </summary>
+    public class TableLists
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the tables.
+        /// </summary>
+        /// <value>The tables.</value>
+        public string Tables { get; set; }
+        /// <summary>
+        /// Gets or sets the display name.  Called DN in the table
+        /// </summary>
+        /// <value>The display name.</value>
+        public string DisplayName { get; set; }
+    }
+}
