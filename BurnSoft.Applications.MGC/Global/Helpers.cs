d1f4308b313fc2081649ba43c4954e669b518fb3
Joe M <bs.github@burnsoft.net>
2021-07-16 10:43:23 -0400

    #31 Added code from Global Class section into helpers

---
+++ BurnSoft.Applications.MGC/Global/Helpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.Global
+{
+    /// <summary>
+    /// Class Helpers.
+    /// </summary>
+    public class Helpers
+    {
+        /// <summary>
+        /// Formats from XML.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>System.String.</returns>
+        public static string FormatFromXML(string value)
+        {
+            string sAns = value.Replace("&amp;", "&");
+            sAns = sAns.Replace("'", "''");
+            return sAns;
+        }
+
+    }
+}
