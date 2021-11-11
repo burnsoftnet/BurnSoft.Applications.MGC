bd40377b9f12771c02a52c54022456449701234f
Joe M <bs.github@burnsoft.net>
2021-11-11 17:03:46 -0500

    #13 started new class to manage the Gun_Type table

---
+++ BurnSoft.Applications.MGC/Firearms/GunTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.Firearms
+{
+    /// <summary>
+    /// Class GunTypes. This class is to help manage the Gun_Type table.
+    /// </summary>
+    public class GunTypes
+    {
+    }
+}
