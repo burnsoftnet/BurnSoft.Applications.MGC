cd91a7b4b52a5c6a3530ec3eb09f5444f8e240c7
Joe M <bs.github@burnsoft.net>
2021-11-22 11:46:50 -0500

    #33 Added updated function for ducment

--- BurnSoft.Applications.MGC/Firearms/Documents.cs
+++ BurnSoft.Applications.MGC/Firearms/Documents.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.OleDb;
+using System.IO;
 using BurnSoft.Applications.MGC.Types;
 // ReSharper disable UnusedMember.Local
 
@@ -91,7 +93,80 @@ namespace BurnSoft.Applications.MGC.Firearms
             }
             return sAns;
         }
-        
+        /// <summary>
+        /// Gets the document from HDD.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>System.Byte[].</returns>
+        public static byte[] GetDocFromHdd(string filePath, out string errOut)
+        {
+            byte[] btAns = null;
+            errOut = "";
+            try
+            {
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                btAns = br.ReadBytes(Convert.ToInt32(fs.Length));
+                br.Close();
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetDocFromHdd", e);
+            }
+            return btAns;
+        }
+        /// <summary>
+        /// Updates the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="documentId">The document identifier.</param>
+        /// <param name="fileWasSelected">if set to <c>true</c> [file was selected].</param>
+        /// <param name="title">The title.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="category">The category.</param>
+        /// <param name="filePathAndName">Name of the file path and.</param>
+        /// <param name="selectedFileType">Type of the selected file.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool Update(string databasePath,int documentId, bool fileWasSelected,string title, string description, string category,string filePathAndName, int selectedFileType, out string errOut)
+        {
+            bool bAns = false;
+            errOut = "";
+            try
+            {
+                OleDbConnection conn = new OleDbConnection(Database.ConnectionStringOle(databasePath,"MGC.mdb", out errOut));
+                conn.Open();
+                String sql =
+                    "update Gun_Collection_Docs set doc_name=@doc_name,doc_description=@doc_description,doc_cat=@doc_cat where id=@did";
+                if (fileWasSelected) sql = "update Gun_Collection_Docs set doc_name=@doc_name,doc_description=@doc_description,doc_cat=@doc_cat,doc_filename=@doc_filename,doc_file=@doc_file,length=@length,doc_ext=@doc_ext where id=@did";
+
+                OleDbCommand addDoc = conn.CreateCommand();
+                addDoc.CommandText = sql;
+                addDoc.Connection = conn;
+                addDoc.Parameters.Add(new OleDbParameter("@doc_name", title));
+                addDoc.Parameters.Add(new OleDbParameter("@doc_description", description));
+                addDoc.Parameters.Add(new OleDbParameter("@doc_cat", category));
+
+                if (fileWasSelected)
+                {
+                    Byte[] doc = GetDocFromHdd(filePathAndName, out errOut);
+                    addDoc.Parameters.Add(new OleDbParameter("@doc_filename", Path.GetFileName(filePathAndName)));
+                    addDoc.Parameters.Add(new OleDbParameter("@doc_file", doc));
+                    addDoc.Parameters.Add(new OleDbParameter("@length", doc.Length));
+                    addDoc.Parameters.Add(new OleDbParameter("@doc_ext", GetDocType(selectedFileType)));
+                }
+                addDoc.Parameters.Add(new OleDbParameter("@did", documentId));
+                addDoc.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Update", e);
+            }
+            return bAns;
+        }
+
         /// <summary>
         /// Gets the last identifier.
         /// </summary>
