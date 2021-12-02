d1bfe5f77a0cd94c6c3f8e7a1f569082b42b423f
Joe M <bs.github@burnsoft.net>
2021-12-02 14:36:12 -0500

    #22 starting unit test

--- BurnSoft.Applications.MGC.UnitTest/Firearms/PictureTests.cs
+++ BurnSoft.Applications.MGC.UnitTest/Firearms/PictureTests.cs
@@ -1,11 +1,47 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BurnSoft.Applications.MGC.Types;
+using BurnSoft.Applications.MGC.UnitTest.Settings;
+using BurnSoft.Universal;
+
+// ReSharper disable UnusedMember.Local
 
 namespace BurnSoft.Applications.MGC.UnitTest.Firearms
 {
     [TestClass]
     public class PictureTests
     {
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        /// <value>The test context.</value>
+        public TestContext TestContext { get; set; }
+        /// <summary>
+        /// The error out
+        /// </summary>
+        private string _errOut;
+        /// <summary>
+        /// The database path
+        /// </summary>
+        private string _databasePath;
+        /// <summary>
+        /// The gun identifier
+        /// </summary>
+        private long _gunId;
+
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            // Vs2019.GetSetting("", TestContext);
+            _errOut = @"";
+            BSOtherObjects obj = new BSOtherObjects();
+            _databasePath = Vs2019.GetSetting("DatabasePath", TestContext);
+            _gunId = Convert.ToInt32(Vs2019.GetSetting("MyGunCollectionID", TestContext));
+        }
         [TestMethod]
         public void TestMethod1()
         {
