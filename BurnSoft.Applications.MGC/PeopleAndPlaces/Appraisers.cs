feece5f81afdaf6a4bf3be0244de3cd814412315
Joe M <bs.github@burnsoft.net>
2021-12-29 16:35:44 -0500

    #32 updated appraisers

--- BurnSoft.Applications.MGC/PeopleAndPlaces/Appraisers.cs
+++ BurnSoft.Applications.MGC/PeopleAndPlaces/Appraisers.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using BurnSoft.Applications.MGC.Global;
 using BurnSoft.Applications.MGC.Types;
 // ReSharper disable UnusedMember.Local
 
