3c7718ed0de74ce061d0b6350fcde64d9be374b5
burnsoftnet <bs.github@burnsoft.net>
2022-01-26 08:02:54 -0500

    #42 started section and class for the hotfix

---
+++ BurnSoft.Applications.MGC/hotixes/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADODB;
+using System.Data.Odbc;
+
+namespace BurnSoft.Applications.MGC.hotixes
+{
+    /// <summary>
+    /// This Database class uses ADODB for database connection, unlike the Database class in the root
+    ///  which uses odbc for it's connection methods.  This code was converted from the hotfix application.
+    /// </summary>
+    public class Database
+    {
+    }
+}
