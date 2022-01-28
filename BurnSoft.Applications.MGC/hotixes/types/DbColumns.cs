bf9c0f510b8077a51225acd9293633ea3d92c964
burnsoftnet <bs.github@burnsoft.net>
2022-01-28 18:13:57 -0500

    #42 Adding hotfixe column types

---
+++ BurnSoft.Applications.MGC/hotixes/types/DbColumns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.hotixes.types
+{
+    public class DbColumns
+    {
+
+    }
+}
