d498d9247177104f2efac78c6588ada37fba79f4
burnsoftnet <bs.github@burnsoft.net>
2022-01-28 18:27:41 -0500

    #42 added types for database updates

--- BurnSoft.Applications.MGC/hotixes/types/DbAddData.cs
+++ BurnSoft.Applications.MGC/hotixes/types/DbAddData.cs
@@ -1,12 +1,12 @@
-﻿
+﻿using System;
+
 namespace BurnSoft.Applications.MGC.hotixes.types
 {
-    public class DbColumns
+    class DbAddData
     {
         public int HotFixId { get; set; }
         public string Column { get; set; }
         public string Table { get; set; }
         public string Value { get; set; }
-        public string Type { get; set; }
     }
 }
