440444bdc9d6267f50284eeb33b079a6b1c680ed
burnsoftnet <bs.github@burnsoft.net>
2022-01-28 18:17:11 -0500

    #42 added values to list

--- BurnSoft.Applications.MGC/hotixes/types/DbColumns.cs
+++ BurnSoft.Applications.MGC/hotixes/types/DbColumns.cs
@@ -8,6 +8,9 @@ namespace BurnSoft.Applications.MGC.hotixes.types
 {
     public class DbColumns
     {
-
+        public int HotFixId { get; set; }
+        public string Column { get; set; }
+        public string Table { get; set; }
+        public string Value { get; set; }
     }
 }
