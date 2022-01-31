84234d8b4bd553e2a473a2607c4fdb0eaa6ef372
burnsoftnet <bs.github@burnsoft.net>
2022-01-31 12:38:08 -0500

    #1 Added comments

--- BurnSoft.Applications.MGC/hotixes/types/DbAddData.cs
+++ BurnSoft.Applications.MGC/hotixes/types/DbAddData.cs
@@ -2,11 +2,30 @@
 
 namespace BurnSoft.Applications.MGC.hotixes.types
 {
+    /// <summary>
+    /// Class DbAddData.
+    /// </summary>
     class DbAddData
     {
+        /// <summary>
+        /// Gets or sets the hot fix identifier.
+        /// </summary>
+        /// <value>The hot fix identifier.</value>
         public int HotFixId { get; set; }
+        /// <summary>
+        /// Gets or sets the column.
+        /// </summary>
+        /// <value>The column.</value>
         public string Column { get; set; }
+        /// <summary>
+        /// Gets or sets the table.
+        /// </summary>
+        /// <value>The table.</value>
         public string Table { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
         public string Value { get; set; }
     }
 }
--- BurnSoft.Applications.MGC/hotixes/types/DbColumns.cs
+++ BurnSoft.Applications.MGC/hotixes/types/DbColumns.cs
@@ -1,12 +1,35 @@
 ﻿
 namespace BurnSoft.Applications.MGC.hotixes.types
 {
+    /// <summary>
+    /// Class DbColumns.
+    /// </summary>
     public class DbColumns
     {
+        /// <summary>
+        /// Gets or sets the hot fix identifier.
+        /// </summary>
+        /// <value>The hot fix identifier.</value>
         public int HotFixId { get; set; }
+        /// <summary>
+        /// Gets or sets the column.
+        /// </summary>
+        /// <value>The column.</value>
         public string Column { get; set; }
+        /// <summary>
+        /// Gets or sets the table.
+        /// </summary>
+        /// <value>The table.</value>
         public string Table { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
         public string Value { get; set; }
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>The type.</value>
         public string Type { get; set; }
     }
 }
