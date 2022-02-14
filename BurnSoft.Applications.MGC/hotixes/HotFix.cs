a125904872c23abea725de95d7a060ddaf3065db
burnsoftnet <bs.github@burnsoft.net>
2022-02-14 18:16:46 -0500

    #42 Adding to Hotfix 1

--- BurnSoft.Applications.MGC/hotixes/HotFix.cs
+++ BurnSoft.Applications.MGC/hotixes/HotFix.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// ReSharper disable UnusedMember.Local
 
 namespace BurnSoft.Applications.MGC.hotixes
 {
@@ -11,10 +12,71 @@ namespace BurnSoft.Applications.MGC.hotixes
     /// </summary>
     public class HotFix
     {
+
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string _classLocation = "BurnSoft.Applications.MGC.hotixes.HotFix";
+        /// <summary>
+        /// Errors the message for regular Exceptions
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, Exception e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for access violations
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, AccessViolationException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for invalid cast exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, InvalidCastException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message argument exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for argument null exception.
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        #endregion
+        //End Snippet
+        
         /// <summary>
         /// The number of fixes
         /// </summary>
         public const int NumberOfFixes = 9;
 
+        private static bool One(string databasePath,out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                if (!Database.Security.AddPassword(databasePath, out errOut)) throw new Exception(errOut);
+                if (!Database.Management.Tables.Columns.Add(databasePath, "PetLoads", "Gun_Collection", "TEXT(255)","'  '", out errOut))
+                    throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return bAns;
+        }
     }
 }
