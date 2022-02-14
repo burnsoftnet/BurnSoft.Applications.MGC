ded14f1943476b361d29726b90770f8ebaea5edd
burnsoftnet <bs.github@burnsoft.net>
2022-02-14 18:35:32 -0500

    #42 added SetLastUpdate function

--- BurnSoft.Applications.MGC/Global/MyRegistry.cs
+++ BurnSoft.Applications.MGC/Global/MyRegistry.cs
@@ -809,6 +809,33 @@ namespace BurnSoft.Applications.MGC.Global
             return bAns;
         }
         /// <summary>
+        /// Sets the last update.
+        /// </summary>
+        /// <param name="hotfixNumber">The hotfix number.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool SetLastUpdate(int hotfixNumber, out string errOut)
+        {
+            bool bAns = false;
+            errOut = "";
+            try
+            {
+                string strValue = DefaultRegPath + @"\HotFix";
+                RegistryKey myReg = Registry.CurrentUser.CreateSubKey(strValue, RegistryKeyPermissionCheck.Default);
+                if (myReg == null)
+                    myReg = Registry.CurrentUser.CreateSubKey(strValue);
+                myReg.SetValue("LastUpdate",$"{hotfixNumber}");
+                myReg.Close();
+                bAns = true;
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("SetLastUpdate", e);
+            }
+
+            return bAns;
+        }
+        /// <summary>
         /// Gets the last working dir.
         /// </summary>
         /// <param name="errOut">The error out.</param>
