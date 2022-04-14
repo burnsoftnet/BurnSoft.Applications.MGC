aba001b7f7afd73121732fd0f6d43ae88cf47fc2
BurnSoft <bs.github@burnsoft.net>
2022-04-14 11:33:20 -0400

    #44 Added ThirdParty Class

---
+++ BurnSoft.Applications.MGC/ThirdParty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC
+{
+    /// <summary>
+    /// Class Third Party Class for some simply functions that another application can use to interact with the Gun Collection Application. This is mostly based off the My Loader Load Functions
+    /// </summary>
+    public class ThirdParty
+    {
+
+    }
+}
