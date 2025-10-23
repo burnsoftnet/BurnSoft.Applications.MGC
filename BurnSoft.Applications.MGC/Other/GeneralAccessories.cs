3db9c289f55bc1e63aa4d510558e47447acf91e8
BurnSoft <bs.github@burnsoft.net>
2025-10-22 22:53:18 -0400

    #85 Started new Class

---
+++ BurnSoft.Applications.MGC/Other/GeneralAccessories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.Other
+{
+    /// <summary>
+    /// Main Class to handle the General Accessories table.
+    /// </summary>
+    public class GeneralAccessories
+    {
+
+    }
+}
