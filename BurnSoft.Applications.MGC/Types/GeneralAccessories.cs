94f07d3fb17fd2f3355420d442fecb9a7e8fb255
BurnSoft <bs.github@burnsoft.net>
2025-10-22 22:45:57 -0400

    #81 Updated Lists for Gun Collection and General Accessoreies with links

--- BurnSoft.Applications.MGC/Types/GeneralAccessories.cs
+++ BurnSoft.Applications.MGC/Types/GeneralAccessories.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BurnSoft.Applications.MGC.Types
 {
@@ -68,9 +69,36 @@ namespace BurnSoft.Applications.MGC.Types
         /// <value><c>true</c> if this instance is choke; otherwise, <c>false</c>.</value>
         public bool IsChoke { get; set; }
         /// <summary>
+        /// Gets or sets the attached to firearmns list identirifers.
+        /// </summary>
+        /// <value>The attached to firearmns.</value>
+        public List<Linkers> AttachedToFirearmns { get; set; }
+        /// <summary>
         /// Gets or sets the last synchronize. Is called sync_lastupdate in the table
         /// </summary>
         /// <value>The last synchronize.</value>
         public string LastSync { get; set; }
+
+        /// <summary>
+        /// Class Linkers.Information from the linker table
+        /// </summary>
+        public class Linkers
+        {
+            /// <summary>
+            /// Gets or sets the identifier.
+            /// </summary>
+            /// <value>The identifier.</value>
+            public int Id { get; set; }
+            /// <summary>
+            /// Gets or sets the Gun Id that it is attached to.
+            /// </summary>
+            /// <value>The gid.</value>
+            public int Gid { get; set; }
+            /// <summary>
+            /// Gets or sets the General Accessory that is used.
+            /// </summary>
+            /// <value>The aid.</value>
+            public int Aid { get; set; }
+        }
     }
 }
