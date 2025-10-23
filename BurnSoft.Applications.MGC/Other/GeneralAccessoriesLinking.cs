b292fd9eace7378b844a6b5021a632edbd6eb34e
BurnSoft <bs.github@burnsoft.net>
2025-10-23 09:19:31 -0400

    #85 Working on Linking functions

--- BurnSoft.Applications.MGC/Other/GeneralAccessoriesLinking.cs
+++ BurnSoft.Applications.MGC/Other/GeneralAccessoriesLinking.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BurnSoft.Applications.MGC.Firearms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,48 @@ namespace BurnSoft.Applications.MGC.Other
     /// </summary>
     public class GeneralAccessoriesLinking
     {
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string ClassLocation = "BurnSoft.Applications.MGC.Other.GeneralAccessoriesLinking";
+        /// <summary>
+        /// Errors the message for regular Exceptions
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, Exception e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for access violations
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, AccessViolationException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for invalid cast exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, InvalidCastException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message argument exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for argument null exception.
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{ClassLocation}.{functionName} - {e.Message}";
+        #endregion                        
+
         public static bool AttachToFirearm(string databasePath, long id, long gunId, out string errOut)
         {
             bool bAns = false;
@@ -21,15 +64,15 @@ namespace BurnSoft.Applications.MGC.Other
             {
                 string sql = $"INSERT INTO General_Accessories_Link(GID, AID) Values({gunId},{id})";
                 if (!Database.Execute(databasePath, sql, out errOut)) throw new Exception(errOut);
-                sql = ""
+                //bAns = Accessories.Add
                 ////string sql = $"Update General_Accessories set Manufacturer='{manufacturer}',Model='{model}'," +
                 ////             $"SerialNumber='{serialNumber}',Condition='{condition}',Notes='{notes}',Use='{use}'," +
                 ////             $"PurValue={purValue},AppValue={appValue},CIV={iCiv},IC={iIc},sync_lastupdate=Now() where id={id}";
-                bAns = Database.Execute(databasePath, sql, out errOut);
+                //bAns = Database.Execute(databasePath, sql, out errOut);
             }
             catch (Exception e)
             {
-                errOut = ErrorMessage("Update", e);
+                errOut = ErrorMessage("AttachToFirearm", e);
             }
 
             return bAns;
