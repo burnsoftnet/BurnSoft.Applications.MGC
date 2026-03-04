048873c3754323b39dd99b5ae507646a63de882e
BurnSoft <bs.github@burnsoft.net>
2026-03-04 14:52:24 -0500

    #1 started gun collection helper

---
+++ BurnSoft.Applications.MGC/LoadersLog/FirearmHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MGC.LoadersLog
+{
+    /// <summary>
+    /// Class FirearmHelpers Gun Collection Data Helper for the My Loaders Log Application
+    /// </summary>
+    public class FirearmHelpers
+    {
+
+    }
+}
