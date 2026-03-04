bfb09eb6526b39619cb732016f5c2eff756ca570
BurnSoft <bs.github@burnsoft.net>
2026-03-04 14:33:36 -0500

    #140 Added Pointer for Registry

---
+++ BurnSoft.Applications.MGC/LoadersLog/RegistryHelpers.cs
@@ -0,0 +1,27 @@
+﻿using BurnSoft.Applications.MGC.Global;
+
+
+namespace BurnSoft.Applications.MGC.LoadersLog
+{
+    /// <summary>
+    /// Class RegistryHelpers Pointer to halp the My Loaders Log use the MyRegistry Class Functions that it uses
+    /// </summary>
+    public class RegistryHelpers
+    {
+        /// <summary>
+        /// Gets the MGC executable path.
+        /// </summary>
+        /// <param name="errOut">The error out.</param>
+        /// <param name="sDefault">The s default.</param>
+        /// <returns>System.String.</returns>
+        public static string GetMgcExePath(out string errOut, string sDefault = "") => MyRegistry.GetMgcExePath(out errOut, sDefault);
+
+        /// <summary>
+        /// Gets the MGC database path.
+        /// </summary>
+        /// <param name="errOut">The error out.</param>
+        /// <param name="sDefault">The s default.</param>
+        /// <returns>System.String.</returns>
+        public static string GetMGCPath(out string errOut, string sDefault = "") => MyRegistry.GetDatabaseLocation(out errOut, sDefault);
+    }
+}
